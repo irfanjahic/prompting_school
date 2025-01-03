914a1a9068cf5f759214fd7c018a1e70ab51156f
Irfan Jahic <irfan.jahic@student.tugraz.at>
2025-01-03 11:35:32 +0100

    updating url

--- prompting_school/src/routes/Register.svelte
+++ prompting_school/src/routes/Register.svelte
@@ -8,7 +8,8 @@
 
     async function handleSubmit() {
         try {
-            const response = await fetch('http://localhost:5000/register', {
+            //const response = await fetch('http://localhost:5000/register', {
+            const response = await fetch('https://api.irfanjahic.com/register', {    
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
